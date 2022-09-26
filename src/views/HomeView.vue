101aeb6e434a25aa78d4c6fa56aba6f5e83d15d1
superboo0311 <60676245+superboo0311@users.noreply.github.com>
2022-09-26 14:59:49 -0400

    init commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div class="w-full flex justify-center">
+    <input type="text" placeholder="Enter Pokemon here" class="mt-10 p-2
+     border-blue-500 border-2" 
+      v-model="text"/>
   </div>
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div class="ml-4 text-2xl text-blue-500" 
+      v-for="(pokemon,idx) in filteredPokemon"
+      :key="idx">
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
+      {{pokemon.name}}
+      </router-link>
+    </div>
+  </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { computed } from "@vue/reactivity";
+import {reactive, toRefs} from "vue";
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  setup(){
+    const state = reactive({
+      pokemons:[],
+      urlIdLookup:{},
+      text:"",
+      filteredPokemon:computed(()=>updatePokemon())
+    })
+    function updatePokemon(){
+        if(!state.text){
+          return []
+        }
+        return state.pokemons.filter((pokemon)=>
+        pokemon.name.includes(state.text)
+        )
+    }
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then((res)=>res.json())
+      .then((data)=>{
+        console.log(data)
+        state.pokemons=data.results;
+        state.urlIdLookup=data.results.reduce((acc,cur,idx)=>
+        acc={...acc,[cur.name]:idx+1},{})
+      })
+      return {...toRefs(state)}
+    }
 }
 </script>
